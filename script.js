f614b3b422b626c81c33ffe7fee200985ed54e9c
Fiza-Rani-057 <fahadsaeed057@gmail.com>
2026-06-23 23:05:22 +0500

    Math EXpressions

---
+++ script.js
@@ -0,0 +1,12 @@
+//  Question 1
+
+ document.write( "<h1>" + "Question 1" + "<br>"  + "</h1>");
+
+ var num = 10;
+
+ document.write("Result:" + "<br>");
+ document.write("The Result of a is:" + num);
+
+//  Question 2
+ document.write( "<h1>" + "Question 2" + "<br>"  + "</h1>");
+ 
