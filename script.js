ffba8bc440d21bf4e13143bd5554a18606489a06
Fiza-Rani-057 <fahadsaeed057@gmail.com>
2026-06-24 00:38:42 +0500

    Maths Expressions

--- script.js
+++ script.js
@@ -19,3 +19,18 @@
 
  document.write("The Value of a-- is:" + (a--) + "<br>");
  document.write("Now the value of a is:" + a  +"<br>" +"<br>"); 
+
+//  Question 2 
+ 
+document.write( "<h1>" + "Question 2" + "<br>"  + "</h1>");
+
+ var b = 1, c = 2;
+ var result = --c  - --b +  ++b +  b--; 
+
+ document.write("a = " + c + "<br>");
+ document.write("b = " + b + "<br>");
+ document.write("Result:" + result);
+
+
+    
+ 
