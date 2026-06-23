38b8c6fe9528adb12e42da146ef76cf5a1eae6bd
Fiza-Rani-057 <fahadsaeed057@gmail.com>
2026-06-24 01:10:55 +0500

    Maths Expressions

--- script.js
+++ script.js
@@ -39,7 +39,21 @@ document.write( "<h1>" + "Question 3" + "<br>"  + "</h1>");
 
  document.write("Hello"  + " "+ userName);
 
-//  Question 4
+//  Question 4 & 5
 
  document.write( "<h1>" + "Question 4" + "<br>"  + "</h1>");
- 
+
+ var number = parseInt(prompt("Enter a number :", "5"));
+
+ document.write(number* 1 + "<br>");
+ document.write(number* 2 + "<br>");
+ document.write(number* 3 + "<br>");
+ document.write(number* 4 + "<br>");
+ document.write(number* 5 + "<br>");
+ document.write(number* 6 + "<br>");
+ document.write(number* 7 + "<br>");
+ document.write(number* 8 + "<br>");
+ document.write(number* 9 + "<br>");
+ document.write(number* 10 + "<br>");
+
+
